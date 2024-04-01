9ab9490a8f9fcc8881820c3fe896872192cb4046
Abdullah Nazir <Abdullah.nazir289@gmail.com>
2024-04-01 23:53:03 +0500

    Update StackImpl.cpp

--- Implementations/StackImpl.cpp
+++ Implementations/StackImpl.cpp
@@ -1,3 +1,12 @@
+// ********************************************************
+// Name: Abdullah Nazir
+// Date: March 27, 2024
+//
+// Implementation file StackImpl.cpp
+// This file contains definitions of the functions declared
+//  in the abstract class Stack .
+// ********************************************************
+
 #include "../Headers/Stack.h"
 
 template <class T>
