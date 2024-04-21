d031d3ac4079bbf3917bb3fb8a50ed953cb2b0d8
Abdullah Nazir <Abdullah.nazir289@gmail.com>
2024-04-22 00:10:41 +0500

    Added SinglyLinkedList with lots of insertion and deletion methods

---
+++ Headers/SinglyLinkedList.h
@@ -0,0 +1,182 @@
+// ********************************************************
+// Programmer: Abdullah Nazir
+// Date: April 22, 2024
+//
+// Class SinglyLinkedList
+// This class contains members and methodsto implement
+// data structure singly linked list. This class
+// is inherited from abstract class LinkedList .
+// ********************************************************
+
+#ifndef H_SinglyLinkedList
+#define H_SinglyLinkedList
+
+// #include "Node.h"
+#include "LinkedList.h"
+
+template <class T>
+class SinglyLinkedList : public LinkedList<T>
+{
+public:
+    SinglyLinkedList();
+    // Default constructor for SinglyLinkedList.
+    // Initializes the head pointer to nullptr.
+
+    void display() const override;
+    // Function to display all contents of the singly
+    //  linked list.
+    // Overrides the display method of the base class.
+
+    void displayFormatted() const override;
+    // Function to display the contents of the singly
+    //  linked list with formatting.
+    // Overrides the displayFormatted method of the base
+    //  class.
+
+    void displayAtIndex(int) override;
+    // Function to display the element at a specific
+    //  index in the singly linked list.
+    // Overrides the displayAtIndex method of the base
+    //  class.
+
+    void insertAtStart(const T &) override;
+    // Function to insert a value at the beginning of
+    //  the singly linked list.
+    // Overrides the insertAtStart method of the base
+    //  class.
+
+    void insertAtStart(Node<T> *) override;
+    // Function to insert a node at the beginning of
+    //  the singly linked list.
+    // Overrides the insertAtStart method of the base
+    //  class.
+
+    void insertAtEnd(const T &) override;
+    // Function to insert a value at the end of the
+    //  singly linked list.
+    // Overrides the insertAtEnd method of the base
+    //  class.
+
+    void insertAtEnd(Node<T> *) override;
+    // Function to insert a node at the end of the
+    //  singly linked list.
+    // Overrides the insertAtEnd method of the base
+    //  class.
+
+    void insertAtIndex(const T &, int) override;
+    // Function to insert a value at a specific index
+    //  in the singly linked list.
+    // Overrides the insertAtIndex method of the base
+    //  class.
+
+    void insertAtIndex(Node<T> *, int) override;
+    // Function to insert a node at a specific index
+    //  in the singly linked list.
+    // Overrides the insertAtIndex method of the base
+    //  class.
+
+    void insertBeforeNode(const T &, Node<T> *) override;
+    // Function to insert a value before a specific
+    //  node in the singly linked list.
+    // Overrides the insertBeforeNode method of the
+    //  base class.
+
+    void insertBeforeNode(Node<T> *, Node<T> *) override;
+    // Function to insert a node before a specific
+    //  node in the singly linked list.
+    // Overrides the insertBeforeNode method of the
+    //  base class.
+
+    void insertAfterNode(const T &, Node<T> *) override;
+    // Function to insert a value after a specific
+    //  node in the singly linked list.
+    // Overrides the insertAfterNode method of the
+    //  base class.
+
+    void insertAfterNode(Node<T> *, Node<T> *) override;
+    // Function to insert a node after a specific
+    //  node in the singly linked list.
+    // Overrides the insertAfterNode method of the
+    //  base class.
+
+    void insertMultiple(T *, int) override;
+    // Function to insert multiple values into the
+    //  singly linked list (start).
+    // Overrides the insertMultiple method of the
+    //  base class.
+
+    void insertMultiple(Node<T> *, int) override;
+    // Function to insert multiple nodes into the
+    //  singly linked list.
+    // Overrides the insertMultiple method of the
+    //  base class.
+
+    void deleteFirst() override;
+    // Function to delete the first node from the
+    //  singly linked list.
+    // Overrides the deleteFirst method of the base
+    //  class.
+
+    void deleteLast() override;
+    // Function to delete the last node from the
+    //  singly linked list.
+    // Overrides the deleteLast method of the base
+    //  class.
+
+    void deleteFromIndex(int) override;
+    // Function to delete a node at a specific index
+    //  from the singly linked list.
+    // Overrides the deleteFromIndex method of the
+    //  base class.
+
+    void deleteByValue(const T &) override;
+    // Function to delete the first node with a specific
+    //  value from the singly linked list.
+    // Overrides the deleteByValue method of the base
+    //  class.
+
+    void deleteAllByValue(const T &) override;
+    // Function to delete all nodes with a specific
+    //  value from the singly linked list.
+    // Overrides the deleteAllByValue method of the
+    //  base class.
+
+    void deleteBeforeNode(Node<T> *) override;
+    // Function to delete the node before a specific
+    //  node from the singly linked list.
+    // Overrides the deleteBeforeNode method of the
+    //  base class.
+
+    void deleteAfterNode(Node<T> *) override;
+    // Function to delete the node after a specific
+    //  node from the singly linked list.
+    // Overrides the deleteAfterNode method of the
+    //  base class.
+
+    void clear() override;
+    // Function to clear the singly linked list
+    //  (remove all nodes but keep the list structure).
+    // Overrides the clear method of the base class.
+
+    void deleteList() override;
+    // Function to delete the entire singly linked
+    //  list (remove all nodes and deallocate memory).
+    // Overrides the deleteList method of the base class.
+
+    void deleteRange(int, int) override;
+    // Function to delete nodes within a specified
+    //  range of indices from the singly linked list.
+    // Overrides the deleteRange method of the base class.
+
+    ~SinglyLinkedList();
+    // Destructor function for SinglyLinkedList.
+    // Performs necessary cleanup, such as deleting all
+    //  nodes and deallocating memory.
+
+private:
+    Node<T> *tail;
+};
+
+#include "../Implementations/SinglyLinkedListImpl.cpp"
+
+#endif // H_SinglyLinkedList
---
+++ Implementations/SinglyLinkedListImpl.cpp
@@ -0,0 +1,662 @@
+// ********************************************************
+// Programmer: Abdullah Nazir
+// Date: April 21, 2024
+//
+// Implementation file SinglyLinkedListImpl.cpp
+// This file contains implementations of the functions
+//  declared in the class SinglyLinkedList .
+// ********************************************************
+
+#include "../Headers/SinglyLinkedList.h"
+#include <iostream>
+
+template <class T>
+SinglyLinkedList<T>::SinglyLinkedList()
+{
+    this->tail = nullptr;
+}
+
+template <class T>
+void SinglyLinkedList<T>::display() const
+{
+    if (!this->head)
+    {
+        std::cout << "Linked List is empty.\n";
+        return;
+    }
+
+    std::cout << "Linked List Data: ";
+    Node<T> *temp = this->head;
+    while (temp->next != nullptr)
+    {
+        std::cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    std::cout << temp->data << "\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::displayFormatted() const
+{
+    if (!this->head)
+    {
+        std::cout << "Linked List is empty.\n";
+        return;
+    }
+
+    std::cout << "[ ";
+    Node<T> *temp = this->head;
+    while (temp->next != nullptr)
+    {
+        std::cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    std::cout << temp->data << " ]\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::displayAtIndex(int index)
+{
+    if (index < 0 || index >= this->nodesCount)
+    {
+        std::cout << "Invalid index.\n";
+        return;
+    }
+
+    int count = 0;
+    Node<T> *temp = this->head;
+    while (temp)
+    {
+        if (count == index)
+        {
+            std::cout << temp->data << '\n';
+            return;
+        }
+        temp = temp->next;
+        count++;
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtStart(Node<T> *node)
+{
+    if (!node)
+    {
+        std::cout << "Cannot insert an empty node.\n";
+        return;
+    }
+    if (!this->head)
+    {
+        this->head = node;
+        this->tail = node;
+        node->next = nullptr;
+    }
+    else
+    {
+        node->next = this->head;
+        this->head = node;
+    }
+    this->nodesCount++;
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtStart(const T &value)
+{
+    Node<T> *temp = new Node<T>(value);
+    insertAtStart(temp);
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtEnd(Node<T> *node)
+{
+    if (!this->head)
+    {
+        this->head = node;
+        this->tail = node;
+    }
+    else
+    {
+        this->tail->next = node;
+        this->tail = node;
+    }
+    this->nodesCount++;
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtEnd(const T &value)
+{
+    Node<T> *temp = new Node<T>(value);
+    insertAtEnd(temp);
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtIndex(Node<T> *value, int index)
+{
+    if (index < 0 || index > this->nodesCount)
+    {
+        std::cout << "Invalid index.\n";
+        return;
+    }
+    if (index == 0)
+        insertAtStart(value);
+    else if (index == this->nodesCount)
+        insertAtEnd(value);
+    else
+    {
+        Node<T> *temp = this->head;
+        for (int count = 0; count < index - 1; count++)
+        {
+            temp = temp->next;
+        }
+        value->next = temp->next;
+        temp->next = value;
+        this->nodesCount++;
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAtIndex(const T &value, int index)
+{
+
+    Node<T> *valueNode = new Node<T>(value);
+    insertAtIndex(valueNode, index);
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertBeforeNode(
+    Node<T> *value,
+    Node<T> *nodeToCheck)
+{
+    if (!this->head)
+    {
+        std::cout << "Linked List is empty. (Not inserted)\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    Node<T> *prev = nullptr;
+    while (temp != nullptr)
+    {
+        if (temp == nodeToCheck)
+        {
+            if (prev == nullptr) // If nodeToCheck is the this->head
+                insertAtStart(value);
+            else
+            {
+                Node<T> *newValue = new Node<T>(*value);
+                prev->next = newValue;
+                newValue->next = temp;
+                this->nodesCount++;
+                return;
+            }
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+
+    std::cout << "Node not found in Linked List.\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertBeforeNode(
+    const T &value,
+    Node<T> *nodeToCheck)
+{
+    Node<T> *valueNode = new Node<T>(value);
+    insertBeforeNode(valueNode, nodeToCheck);
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAfterNode(
+    Node<T> *value,
+    Node<T> *nodeToCheck)
+{
+    if (!this->head)
+    {
+        std::cout << "Linked List is empty. (Not added)\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp != nullptr)
+    {
+        if (temp == nodeToCheck)
+        {
+            if (this->tail == temp)
+                this->tail = value;
+            else
+                value->next = temp->next;
+            temp->next = value;
+            this->nodesCount++;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    std::cout << "Node not found in Linked List.\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertAfterNode(
+    const T &value,
+    Node<T> *nodeToCheck)
+{
+    Node<T> *valueNode = new Node<T>(value);
+    insertAfterNode(valueNode, nodeToCheck);
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertMultiple(Node<T> *valuesList, int count)
+{
+    // Assuming that the valuesList is also a linked list.
+    if (!valuesList)
+    {
+        std::cout << "Cannot insert empty Linked List.\n";
+        return;
+    }
+
+    if (!this->head)
+    {
+        this->head = valuesList;
+        this->nodesCount = count;
+    }
+    else
+    {
+        Node<T> *temp = valuesList;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = this->head;
+        this->head = valuesList;
+        this->nodesCount += count;
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::insertMultiple(T *values, int count)
+{
+    if (!values || count <= 0)
+    {
+        std::cout << "Cannot add empty data.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+
+    while (temp && temp->next)
+    {
+        temp = temp->next;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        Node<T> *valueNode = new Node<T>(values[i]);
+        if (!temp)
+            this->head = valueNode;
+        else
+            temp->next = valueNode;
+        temp = valueNode;
+        this->nodesCount++;
+    }
+    this->tail = temp;
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteFirst()
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    this->head = this->head->next;
+    if (!this->head)
+        this->tail = nullptr;
+    delete temp;
+    this->nodesCount--;
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteLast()
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    if (!this->head->next)
+    {
+        delete this->head;
+        this->nodesCount--;
+        this->head = nullptr;
+        this->tail = nullptr;
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    this->nodesCount--;
+    temp->next = nullptr;
+    this->tail = temp;
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteFromIndex(int index)
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    if (index < 0 || index >= this->nodesCount)
+    {
+        std::cout << "Invalid index.\n";
+        return;
+    }
+
+    if (index == 0)
+    {
+        deleteFirst();
+        return;
+    }
+
+    if (index == this->nodesCount - 1)
+    {
+        deleteLast();
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    for (int i = 0; i < index - 1; i++)
+    {
+        temp = temp->next;
+    }
+    Node<T> *toDelete = temp->next;
+    temp->next = temp->next->next;
+    if (!temp->next)
+        this->tail = temp;
+    delete toDelete;
+    this->nodesCount--;
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteByValue(const T &value)
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp->next)
+    {
+        if (temp->next->data == value)
+        {
+            Node<T> *toDelete = temp->next;
+            temp->next = temp->next->next;
+            if (this->head == toDelete)
+                this->head = this->head->next;
+            delete toDelete;
+            if (!temp->next)
+                this->tail = temp;
+            this->nodesCount--;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    std::cout << "Cannot find " << value << " in Linked List.\n";
+}
+
+// template <class T>
+// void SinglyLinkedList<T>::deleteAllByValue(const T &value)
+// {
+//     if (!this->head)
+//     {
+//         std::cout << "Cannot delete from empty Linked List.\n";
+//         return;
+//     }
+
+//     bool flag = false;
+//     Node<T> *temp = this->head;
+//     while (temp->next)
+//     {
+//         if (temp->next->data == value)
+//         {
+//             Node<T> *toDelete = temp->next;
+//             temp->next = temp->next->next;
+//             if (this->head == toDelete)
+//                 this->head = this->head->next;
+//             flag = true;
+//             delete toDelete;
+//             if (!temp->next)
+//                 this->tail = temp;
+//             this->nodesCount--;
+//         }
+//         temp = temp->next;
+//     }
+
+//     // if (this->head->data == value)
+//     // {
+//     //     Node<T> *toDelete = this->head;
+//     //     this->head = this->head->next;
+//     //     delete toDelete;
+//     //     if (!this->head)
+//     //         this->tail = nullptr;
+//     //     this->nodesCount--;
+//     //     flag = true;
+//     // }
+
+//     if (flag)
+//         std::cout << "Delete all occurrences of "
+//                   << value << " in Linked List.\n";
+//     else
+//         std::cout << "Cannot find " << value << " in Linked List.\n";
+// }
+
+template <class T>
+void SinglyLinkedList<T>::deleteAllByValue(const T &value)
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    bool flag = false;
+    Node<T> *temp = this->head;
+    while (temp && temp->next)
+    {
+        if (temp->next->data == value)
+        {
+            Node<T> *toDelete = temp->next;
+            temp->next = temp->next->next;
+            if (this->head == toDelete)
+                this->head = this->head->next;
+            flag = true;
+            delete toDelete;
+            if (!temp->next)
+                this->tail = temp;
+            this->nodesCount--;
+        }
+        temp = temp->next;
+    }
+
+    if (flag)
+        std::cout << "Delete all occurrences of " << value << " in Linked List.\n";
+    else
+        std::cout << "Cannot find " << value << " in Linked List.\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteBeforeNode(Node<T> *nodeToCheck)
+{
+    if (!this->head || !this->head->next)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    if (nodeToCheck == this->head)
+    {
+        std::cout << "Cannot delete before the given node.\n";
+        return;
+    }
+
+    if (nodeToCheck == this->head->next)
+    {
+        deleteFirst();
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp->next->next != nullptr)
+    {
+        if (temp->next->next == nodeToCheck)
+        {
+            // logic to delete.
+            Node<T> *nodeToDelete = temp->next;
+            temp->next = nodeToCheck;
+            delete nodeToDelete;
+            this->nodesCount--;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    std::cout << "Node not found.\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteAfterNode(Node<T> *nodeToCheck)
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp != nullptr)
+    {
+        if (temp == nodeToCheck)
+        {
+            if (temp->next == nullptr)
+            {
+                std::cout << "No node to delete after given node.\n";
+                return;
+            }
+            Node<T> *nodeToDelete = temp->next;
+            temp->next = temp->next->next;
+            delete nodeToDelete;
+            if (temp->next == nullptr)
+                tail = temp;
+            this->nodesCount--;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    std::cout << "Node not found.\n";
+}
+
+template <class T>
+void SinglyLinkedList<T>::clear()
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot clear empty Linked List.\n";
+    }
+    Node<T> *temp = this->head;
+    while (temp)
+    {
+        temp->data = T();
+        temp = temp->next;
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteList()
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete empty Linked List.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    while (temp)
+    {
+        Node<T> *nodeToDelete = temp;
+        temp = temp->next;
+        delete nodeToDelete;
+    }
+    this->head = nullptr;
+    this->tail = nullptr;
+    this->nodesCount = 0;
+}
+
+template <class T>
+void SinglyLinkedList<T>::deleteRange(int startIndex, int endIndex)
+{
+    if (!this->head)
+    {
+        std::cout << "Cannot delete from empty Linked List.\n";
+        return;
+    }
+    if (startIndex < 0 ||
+        endIndex < 0 ||
+        startIndex >= endIndex ||
+        endIndex >= this->nodesCount)
+    {
+        std::cout << "Invalid range.\n";
+        return;
+    }
+
+    Node<T> *temp = this->head;
+    Node<T> *prev = nullptr;
+    for (int i = 0; i < startIndex; i++)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    for (int i = startIndex; i < endIndex; i++)
+    {
+        Node<T> *nextNode = temp->next;
+        delete temp;
+        this->nodesCount--;
+        if (prev)
+            prev->next = nextNode;
+        else
+            this->head = nextNode;
+        temp = nextNode;
+    }
+
+    if (!temp)
+        this->tail = prev;
+}
+
+template <class T>
+SinglyLinkedList<T>::~SinglyLinkedList()
+{
+    Node<T> *current = this->head;
+    while (current)
+    {
+        Node<T> *next = current->next;
+        delete current;
+        current = next;
+    }
+    this->head = nullptr;
+    this->tail = nullptr;
+    this->nodesCount = 0;
+}
